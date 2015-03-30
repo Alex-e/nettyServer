0ab29c886ce30f93a68ca1f6e6601a7d9c7a3136
Alex-e <a.ieromenko@gmail.com>
2015-03-31 01:55:30 +0300

    refactor LoggingQueue

--- src/main/java/ua/ieromenko/util/LoggingQueue.java
+++ src/main/java/ua/ieromenko/util/LoggingQueue.java
@@ -11,13 +11,15 @@ import java.util.function.Consumer;
  */
 public final class LoggingQueue<E> implements Iterable<E> {
     private final ConcurrentLinkedQueue<E> queue = new ConcurrentLinkedQueue<>();
+    private int counter = 0;
 
     public Iterator<E> iterator() {
         return queue.iterator();
     }
 
     public synchronized boolean add(E e) {
-        if (queue.size() > 15) queue.remove();
+        counter++;
+        if (counter == 16) queue.remove();
         return queue.add(e);
     }
 
